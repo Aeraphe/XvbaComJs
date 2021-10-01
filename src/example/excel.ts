ad6c2b44a584e8245063e6bd0e9fa2751d08fc3e
Aeraphe <alberto.aeraph@gmail.com>
2021-10-01 17:47:24 -0300

    feat(excel): upadate Excel methods sor accept mult params

--- src/example/excel.ts
+++ src/example/excel.ts
@@ -1,4 +1,3 @@
-
 import { Excel } from "../api/Excel/Excel";
 import { WorkBook } from "../api/Excel/WorkBook";
 import { VBComponent } from "../api/Excel/VBComponent";
@@ -10,61 +9,49 @@ export const tryExcel = async () => {
     excel.Visible();
     let Book: WorkBook = excel.WorkBooks.Open("F:\\Apps\\XvbaCom\\index.xlsb");
 
-    let sheets = excel.Sheets
-    
+    let sheets = excel.Sheets;
+
     let total = sheets.Count();
     console.log("--->", total);
     let exName = excel.Name();
     console.log("--->", exName);
-    let totalComp =Book.VBProject.VBComponents.Count;
-   console.log("--->",totalComp);
+    let totalComp = Book.VBProject.VBComponents.Count;
+    console.log("--->", totalComp);
 
+    for (let index = 0; index < totalComp; index++) {
+      const vbComponent: VBComponent = Book.VBProject.VBComponents.Item(
+        index + 1
+      );
+      const compName = vbComponent.Name;
+      let compFileName = "";
 
-
-   for (let index = 0; index < totalComp; index++) {
-
-
-    const vbComponent :VBComponent = Book.VBProject.VBComponents.Item(index  + 1);
-    const compName = vbComponent.Name
-    let compFileName = ""
-
-    switch (vbComponent.Type) {
-
+      switch (vbComponent.Type) {
         case 1:
-            compFileName = compName + ".bas"
-            break;
+          compFileName = compName + ".bas";
+          break;
         case 2:
-            compFileName = compName + ".cls"
-            break;
+          compFileName = compName + ".cls";
+          break;
         case 3:
-            compFileName = compName + ".frm"
-            break;
+          compFileName = compName + ".frm";
+          break;
         case 100:
-            compFileName = compName + ".cls"
-            break;
-
+          compFileName = compName + ".cls";
+          break;
 
         default:
-            break;
+          break;
+      }
+
+      const fileExport = "F:\\Apps\\XvbaCom\\vba\\" + compFileName;
+
+      vbComponent.Export(fileExport);
     }
 
-    setTimeout(() => {
-      
-    }, 3000);
-    const fileExport  = "F:\\Apps\\XvbaCom\\vba\\" + compFileName;
-
-    vbComponent.Export(fileExport);
-
-
-}
-
-
-
+    excel.Quit();
     Excel.CloseAllCOMWithDelay();
   } catch (error) {
     console.log(error);
-    Excel.CloseAllCOM();
+    Excel.CloseAllCOMWithDelay();
   }
 };
-
-
